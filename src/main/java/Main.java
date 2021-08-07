bae927ec233679ec01154b1f5e5b21be40a81462
Jbasilious <Jumbler@my.yorku.ca>
2021-08-07 17:34:24 -0400

    initial setup / cleaning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,30 @@ import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
+import discord4j.core.object.entity.channel.MessageChannel;
+import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 public class Main {
-
     private static final Map<String, Command> commands = new HashMap<>();
     static {
         commands.put("hello",event -> event.getMessage().getChannel()
                 .flatMap(channel -> channel.createMessage("Hi, I'm Mischief Bot"))
                 .then());
+        commands.put("help",event -> event.getMessage().getChannel()
+                .flatMap(channel -> channel.createMessage("Please visit https://github.com/Jbasilious/MischiefBot for a information on commands"))
+                .then());
     }
 
-    public static void main(String[] args) {
-        GatewayDiscordClient client = DiscordClientBuilder.create(Config.BOT_TOKEN)
+    public void main(String[] args) {
+   GatewayDiscordClient client = DiscordClientBuilder.create(Config.BOT_TOKEN)// replace Config.BOT_TOKEN with your own bot token
                 .build()
                 .login()
                 .block();
@@ -29,14 +35,14 @@ public class Main {
 
         client.getEventDispatcher().on(ReadyEvent.class).subscribe(event -> {
             final User self = event.getSelf();
-            System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
+            System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
         });
 
 
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 .flatMap(message -> Mono.just(message.getMessage().getContent())
                 .flatMap(content -> Flux.fromIterable(commands.entrySet())
-                .filter(entry -> content.startsWith('!' + entry.getKey()))
+                .filter(entry -> content.startsWith(">" + entry.getKey()))
                 .flatMap(entry -> entry.getValue().execute(message))
                 .next()))
                 .subscribe();
